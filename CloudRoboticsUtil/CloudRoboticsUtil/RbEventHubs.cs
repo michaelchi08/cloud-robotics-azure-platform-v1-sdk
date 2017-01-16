26bfba75a088fc9bc03cfac1e6d8730181a7bd26
Seiji Doku Miyahara <seijim.ms@live.jp>
2017-01-16 17:14:07 +0900

    Add files via upload

---
+++ CloudRoboticsUtil/CloudRoboticsUtil/RbEventHubs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using Microsoft.ServiceBus.Messaging;
+
+
+namespace CloudRoboticsUtil
+{
+    public class RbEventHubs
+    {
+        private string eventHubConnString;
+        private string eventHubName;
+
+        public RbEventHubs(string eventHubConnString, string eventHubName)
+        {
+            this.eventHubConnString = eventHubConnString;
+            this.eventHubName = eventHubName;
+        }
+
+        public void SendMessage(string message, string deviceId)
+        {
+            var eventHubClient = EventHubClient.CreateFromConnectionString(eventHubConnString, eventHubName);
+            EventData eventData = new EventData(Encoding.UTF8.GetBytes(message));
+            eventData.PartitionKey = deviceId;
+            eventHubClient.SendAsync(eventData);
+        }
+
+    }
+}
